c3c1b2daffed68e0ec06c433cad14c0a56ae0e3f
PoseaC <catalin.posea@stud.fils.upb.ro>
2025-07-27 01:20:47 +0300

    rewritten styling using tailwind-css for responsive design

--- src/index.tsx
+++ src/index.tsx
@@ -19,29 +19,36 @@ class Menu extends React.Component {
 
     render(): React.JSX.Element {
         return (
-            <div className='menu'>
-                <div className="title font">
+            <div className='place-self-center grid grid-cols-1 grid-rows-3 m-8 md:grid-rows-2'>
+                <div className="title font row-1 place-self-center">
                     <h1>Alexandra & Iustin 2026</h1>
                     <h2>
                         &#xCE;nc&#x103;rca&#x21B;i poze &#x15F;i videoclipuri &#xEE;n Galeria Invita&#x21B;ilor!
                     </h2>
                 </div>
 
-                <a href="#" onClick={this.openGallery} className="button">
-                    <div className="icon">
-                        <img src="./img/camera_icon.png" alt="" className="camera"/>
-                    </div>
-                    <p className="font">Selecteaz&#x103;</p>
-                </a>
+                <div className='row-span-2 md:row-span-1 grid grid-cols-1 justify-items-center md:grid-cols-3 gap-4 mb-16 align-middle'>
+                    <a href="#" onClick={this.openGallery} className="button w-full max-w-sm align-middle grid grid-rows-1 rounded-xl p-4">
+                        <div className="icon justify-self-end row-1 mr-2 self-center">
+                            <img src="./img/camera_icon.png" alt="" className="camera"/>
+                        </div>
+                        <p className="font row-1 self-center">Selecteaz&#x103;</p>
+                    </a>
 
-                <div id="placeholder_image"></div>
+                    <a href="#" onClick={this.openGallery} className="button w-full max-w-sm align-middle grid grid-rows-1 rounded-xl p-4">
+                        <div className="icon justify-self-end row-1 mr-2 self-center">
+                            <img src="./img/gallery.svg" alt="" className="gallery"/>
+                        </div>
+                        <p className="font row-1 self-center">Galerie</p>
+                    </a>
 
-                <a href="./success.html" onClick={this.openGallery} className="button">
-                    <div className="icon">
-                        <img src="./img/send_icon.png" alt="" className="send"/>
-                    </div>
-                    <p className="font">Trimite</p>
-                </a>
+                    <a href="./success.html" onClick={this.openGallery} className="button w-full max-w-sm align-middle grid grid-rows-1 rounded-xl p-4">
+                        <div className="icon justify-self-end row-1 mr-2 self-center">
+                            <img src="./img/send_icon.png" alt="" className="send"/>
+                        </div>
+                        <p className="font row-1 self-center">Trimite</p>
+                    </a>
+                </div>
             </div>
         );
     }
